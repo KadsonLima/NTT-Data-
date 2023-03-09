c5715b63943a4ff90dc6c0f892f746e86a6c60f2
Kadson Lima <kadson-kaio@hotmail.com>
2023-03-09 11:07:03 -0300

    hello world

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header } from '../components/header';
+import './App.css';
+
+function App() {
+  return (
+    <> 
+    <Header/>
+    <div className="App">
+      
+    </div>
+  </>
+   
+  );
+}
+
+export default App;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,9 @@
+import "./styles.scss"
+
+export const Header = () =>{
+
+
+    return <div>
+
+    </div>
+}
