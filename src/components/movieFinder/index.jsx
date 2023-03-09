26c5424a64cee0b9a998c1775c6e5b34bcaf826a
Kadson Lima <kadson-kaio@hotmail.com>
2023-03-09 14:28:02 -0300

    feat: movieFinder clean input

--- src/components/movieFinder/index.jsx
+++ src/components/movieFinder/index.jsx
@@ -2,14 +2,17 @@ import "./styles.scss"
 
 export const MovieFinder = () =>{
 
+    const cleanInput = () =>{
+        this.inputTitle.value = "";
+    }
 
     return <div className="MovieFinder">
         <h1>Title</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
         <div className="finder">
-            <input></input>
+            <input />
             <button>Search</button>
-            <button>Reset</button>
+            <button onClick={()=>cleanInput}>Reset</button>
         </div>
     </div>
 }
